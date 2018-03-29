c305eb2fffae9af1d667a66e9b07428bb79acd38
Rik1137 <37886290+Rik1137@users.noreply.github.com>
2018-03-28 20:17:47 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class btnWriteToFile : Form
+    {
+        Lab2.GameFactory gameFactory;
+
+        public btnWriteToFile()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gameFactory = new Lab2.GameFactory();
+
+            gameFactory.CreateGameList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gameFactory.FilePath = @"..\..\games.xml";
+            gameFactory.SerializeGameList();
+
+        }
+
+        private void btnReadFromFile_Click(object sender, EventArgs e)
+        {
+
+
+            gameFactory.DeserializeGameList();
+            
+            foreach(Lab2.Game g in gameFactory.listOfGames)
+            {
+                listBox1.Items.Add(g.ToString());
+            }
+            
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Lab2
+{
+    public class Game
+    {
+        public string team1 { get; set; }
+        public string team2 { get; set; }
+        public int team1Score { get; set; }
+        public int team2Score { get; set; }
+
+        public Game()
+        {
+
+        }
+
+        public Game(string team1, string team2, int team1Score, int team2Score)
+        {
+            this.team1 = team1;
+            this.team2 = team2;
+            this.team1Score = team1Score;
+            this.team2Score = team2Score;
+        }
+
+        public override string ToString()
+        {
+            String message = team1 + " (" + team1Score + ") " + "- " + team2 + " (" + team2Score + ") ";
+            return message;
+        }
+
+
+    }
+
+    public class GameFactory
+    {
+        public List<Game> listOfGames;
+
+        public string FilePath { get; set; }
+        
+
+        public void CreateGameList()
+        {
+            listOfGames = new List<Game>();
+
+            Game game = new Game("Hornets", "Bulldogs", 10, 20);
+            listOfGames.Add(game);
+
+            game = new Game("Sharks", "Bulldogs", 10, 20);
+            listOfGames.Add(game);
+
+            game = new Game("Hornets", "Sharks", 10, 20);
+            listOfGames.Add(game);
+
+            game = new Game("Panthers", "Sharks", 10, 20);
+            listOfGames.Add(game);
+
+            game = new Game("Hornets", "Panthers", 10, 20);
+            listOfGames.Add(game);
+
+            game = new Game("Lions", "Dogs", 10, 20);
+            listOfGames.Add(game);
+
+
+
+        }
+
+        public bool SerializeGameList()
+        {
+            StreamWriter sw = new StreamWriter(FilePath);
+            XmlSerializer serial = new XmlSerializer(listOfGames.GetType());
+            serial.Serialize(sw, listOfGames);
+            sw.Close();
+
+            return true;
+            
+        }
+
+        public List<Game> DeserializeGameList()
+        {
+            listOfGames = new List<Game>();
+
+            StreamReader sr = new StreamReader(FilePath);
+            XmlSerializer serial = new XmlSerializer(listOfGames.GetType());
+            
+            listOfGames = (List<Game>)serial.Deserialize(sr);
+            sr.Close();
+
+            return listOfGames;
+        }
+    }
+}
